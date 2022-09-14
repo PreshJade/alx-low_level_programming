fcbca874e1cc8216b61bdf4fc8781b7364de859e
Aver Precious Fanyam <apfanyam@gmail.com>
2022-09-14 16:27:44 -0700

    my second work

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet
+ *
+ * Return: always 0;
+ *
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+_putchar('\n');
+}
