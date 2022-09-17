5a3e5eee46eecc9fbf93b40cf17e414906f823fb
Aver Precious Fanyam <apfanyam@gmail.com>
2022-09-16 17:50:13 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - returns the product of its parameters
+ * @a: int type numbers
+ * @b: int type numbers
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return(a * b);
+}
