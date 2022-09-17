f61f985f24ec67a0cc3dfe72ce1321057aa80e7e
Aver Precious Fanyam <apfanyam@gmail.com>
2022-09-16 18:03:44 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * ;print_most_numbers - Print numbers 0 to 9, excludinng 2 and 4.
+ * Print_most_numbers - Print numbers 0 to 9, excludinng 2 and 4.
  * Description: Can only use _putchar twice.
  */
 void print_most_numbers(void)
